9e388a32fe02b9c4dca38704247726a215952121
Hakim Akbaru Sulthony <hakimakbar21@gmail.com>
2022-06-21 21:07:37 +0700

    Fixing Delete Function

--- resources/views/hotel/index.blade.php
+++ resources/views/hotel/index.blade.php
@@ -34,10 +34,10 @@
                             <tr>
                                 <td>
                                     <a href="{{ route('hotel.show', $hotel->id) }}">
-                                        {{mb_strimwidth($hotel->name, 0, 50, "...")}} 
-                                    </a> 
+                                        {{mb_strimwidth($hotel->name, 0, 50, "...")}}
+                                    </a>
                                 </td>
-                                <td> <?php echo mb_strimwidth($hotel->address, 0, 20, "..."); ?> </td>
+                                <td> <?php echo mb_strimwidth($hotel->address, 0, 40, "..."); ?> </td>
                                 <td> {{mb_strimwidth($hotel->regency, 0, 20, "...")}} </td>
                                 <td> {{mb_strimwidth($hotel->phone, 0, 20, "...")}} </td>
                                 <td> <a href="{{$hotel->website}}" target="_blank">{{mb_strimwidth($hotel->website, 0, 20, "...")}}</a> </td>
@@ -46,11 +46,9 @@
                                         <a href="{{ route('hotel.edit', $hotel->id) }}" class="btn btn-success btn-icons btn-rounded">
                                             <i class="mdi mdi-pencil"></i>
                                         </a>
-                                        <a href="{{ route('hotel.destroy', $hotel->id) }}" class="btn btn-danger btn-icons btn-rounded" onclick="return confirm('Are you sure?')">
+                                        <a href="{{ route('hotel.destroy', $hotel->id) }}" class="btn btn-danger btn-icons btn-rounded" onclick="notificationBeforeDelete(event, this)">
                                             <i class="mdi mdi-delete"></i>
                                         </a>
-                                        {!! method_field('delete') !!}
-                                        {!! csrf_field() !!}
                                     </form>
                                 </td>
                             </tr>
@@ -78,11 +76,23 @@
 @endpush
 
 @push('custom-scripts')
+<form action="" id="delete-form" method="post">
+    {!! method_field('delete') !!}
+    {!! csrf_field() !!}
+</form>
 <script src="https://code.jquery.com/jquery-3.1.0.js"></script>
 <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
 <script>
     $('#example2').DataTable({
         "responsive": true,
     });
+
+    function notificationBeforeDelete(event, el) {
+        event.preventDefault();
+        if (confirm('Apakah anda yakin akan menghapus data ? ')) {
+            $("#delete-form").attr('action', $(el).attr('href'));
+            $("#delete-form").submit();
+        }
+    }
 </script>
 @endpush
